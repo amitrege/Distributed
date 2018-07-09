bbf15784a5e046d095dadc5d7d0dd28d14db99ef
Amit Kiran Rege <amitrege96@gmail.com>
2018-07-09 18:15:41 +0200

    Fix zab

--- zab_memcad.c
+++ zab_memcad.c
@@ -923,7 +923,50 @@ int notLeader(int num) {
             rand(&retry);
         }
 
-        p = p + 1;
+        if(num_mbox_new_l >= 1) {
+            // Update a
+            // Update history
+
+            lab = 5; // ack_l
+
+            assert((p > old_p) || ((p == old_p) && (lab > old_lab)) || ((p == old_p) && (lab == old_lab) && (i > old_i)) || ((p == old_p) && (lab == old_lab) && (i == old_i) && (labr >= old_labr)));                                        
+            old_p = p;
+            old_lab = lab;
+            old_i = i;
+            old_labr = labr;
+
+            // send (lab, a, h) to leader
+
+            lab = 6; // cmt
+
+            assert((p > old_p) || ((p == old_p) && (lab > old_lab)) || ((p == old_p) && (lab == old_lab) && (i > old_i)) || ((p == old_p) && (lab == old_lab) && (i == old_i) && (labr >= old_labr)));                                        
+            old_p = p;
+            old_lab = lab;
+            old_i = i;
+            old_labr = labr;
+            
+            // receive commit
+            // retry = rand() % 2;
+            rand(&retry);
+            while(retry && num_mbox_com < 1) {
+                if(filter_com(&m_com, p, lab)) {
+                    mbox_com[num_mbox_com] = &m_com;
+                    num_mbox_com = num_mbox_com + 1;
+                }
+    
+                if (num_mbox_com >= 1) {
+                    break;
+                }
+    
+                // retry = rand() % 2;
+                rand(&retry);
+            }
+
+            p = p + 1;
+        }
+        else {
+            p = p + 1;
+        }
     }
     else {
         // Special Case (state transfer?) 
