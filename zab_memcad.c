b2bfb1144be264e13cff9253ca6d1d9101391049
Amit Kiran Rege <amitrege96@gmail.com>
2018-07-09 17:58:15 +0200

    Fix zab

--- zab_memcad.c
+++ zab_memcad.c
@@ -620,7 +620,192 @@ int main_thread(int pid, int num){
     }
 }
 
+int leadership (int num) {
+    int lab = 0;
+    int p = 0;
+    int i = 0;
+    int labr = 0;
+
+    int old_p = p - 1;
+    int old_lab = 0;
+    int old_i = 0;
+    int old_labr = 0;
+
+    int leader;
+
+    msg_curr_e* mbox_curr_e[200];
+    int num_mbox_curr_e = 0;
+    msg_curr_e m_curr_e;
+
+    msg_new_e* mbox_new_e[200];
+    int num_mbox_new_e = 0;
+    msg_new_e m_new_e;
+
+    msg_ack_e* mbox_ack_e[200];
+    int num_mbox_ack_e = 0;
+    msg_ack_e m_ack_e;
+
+    msg_new_l* mbox_new_l[200];
+    int num_mbox_new_l = 0;
+    msg_new_l m_new_l;
+
+    msg_ack_l* mbox_ack_l[200];
+    int num_mbox_ack_l = 0;
+    msg_ack_l m_ack_l;
+
+    msg_com* mbox_com[200];
+    int num_mbox_com = 0;
+    msg_com m_com;
+
+    int retry;
+    
+    lab = 1; // Curr_E
+    
+    //assert((p > old_p) || ((p == old_p) && (lab > old_lab)) || ((p == old_p) && (lab == old_lab) && (i > old_i)) || ((p == old_p) && (lab == old_lab) && (i == old_i) && (labr >= old_labr)));        
+    old_p = p;
+    old_lab = lab;
+    old_i = i;
+    old_labr = labr;
+    
+    // send (p,lab) to leader
+
+    // receive curr_e
+    // retry = rand() % 2;
+    rand(&retry);
+    while(retry && (num_mbox_curr_e < (num/2))) {
+        if(filter_curr_e(&m_curr_e, p, lab)) {
+            mbox_curr_e[num_mbox_curr_e] = &m_curr_e;
+            num_mbox_curr_e = num_mbox_curr_e + 1;
+        }
+
+        if (num_mbox_curr_e >= num/2) {
+            break;
+        }
+
+        // retry = rand() % 2;
+        rand(&retry);
+    }
+    
+    if (num_mbox_curr_e >= num/2) {
+        // Update set Q which is the set of pids in mbox
+        // get max p in mbox and add 1
+
+        rand(&retry);
+        if(retry) {  // Actually, p is the max value of all p's received by the leader
+            p = p + 1;
+        }
+        else {
+            p = p + 2;
+        } 
+        p = p + 1;
+
+        lab = 2; // new_e
+
+        assert((p > old_p) || ((p == old_p) && (lab > old_lab)) || ((p == old_p) && (lab == old_lab) && (i > old_i)) || ((p == old_p) && (lab == old_lab) && (i == old_i) && (labr >= old_labr)));                        
+        old_p = p;
+        old_lab = lab;
+        old_i = i;
+        old_labr = labr;
+
+        //send p to all (in set q)
+
+        lab = 3; // ack_e
+
+        assert((p > old_p) || ((p == old_p) && (lab > old_lab)) || ((p == old_p) && (lab == old_lab) && (i > old_i)) || ((p == old_p) && (lab == old_lab) && (i == old_i) && (labr >= old_labr)));                        
+        old_p = p;
+        old_lab = lab;
+        old_i = i;
+        old_labr = labr;
+
+        // receive ack_e
+        // retry = rand() % 2;
+        rand(&retry);
+        while(retry && (num_mbox_ack_e < (num/2))) {
+            if(filter_ack_e(&m_ack_e, p, lab)) {
+                mbox_ack_e[num_mbox_ack_e] = &m_ack_e;
+                num_mbox_ack_e = num_mbox_ack_e + 1;
+            }
+
+            if (num_mbox_ack_e >= num/2) {
+                break;
+            }
+
+            // retry = rand() % 2;
+            rand(&retry);
+        }
+
+        if(num_mbox_ack_e >= num/2) {
+            // Update history
+
+            lab = 4; // new_l
+
+            assert((p > old_p) || ((p == old_p) && (lab > old_lab)) || ((p == old_p) && (lab == old_lab) && (i > old_i)) || ((p == old_p) && (lab == old_lab) && (i == old_i) && (labr >= old_labr)));                            
+            old_p = p;
+            old_lab = lab;
+            old_i = i;
+            old_labr = labr;
+
+            // send (p, lab, h) to Q
+
+            lab = 5; // ack_l
+
+            assert((p > old_p) || ((p == old_p) && (lab > old_lab)) || ((p == old_p) && (lab == old_lab) && (i > old_i)) || ((p == old_p) && (lab == old_lab) && (i == old_i) && (labr >= old_labr)));                            
+            old_p = p;
+            old_lab = lab;
+            old_i = i;
+            old_labr = labr;
+
+            // receive ack_l
+            // retry = rand() % 2;
+            rand(&retry);
+            while(retry && (num_mbox_ack_l < (num/2))) {
+                if(filter_ack_l(&m_ack_l, p, lab)) {
+                    mbox_ack_l[num_mbox_ack_l] = &m_ack_l;
+                    num_mbox_ack_l = num_mbox_ack_l + 1;
+                }
+    
+                if (num_mbox_ack_l >= num/2) {
+                    break;
+                }
+    
+                // retry = rand() % 2;
+                rand(&retry);
+            }
+            
+            if (num_mbox_ack_l >= num/2) {
+                lab = 6; // cmt
+
+                assert((p > old_p) || ((p == old_p) && (lab > old_lab)) || ((p == old_p) && (lab == old_lab) && (i > old_i)) || ((p == old_p) && (lab == old_lab) && (i == old_i) && (labr >= old_labr)));                                
+                old_p = p;
+                old_lab = lab;
+                old_i = i;
+                old_labr = labr;
+
+                // send (p, lab, h) to Q
+
+                // Start Broadcast
+                //Broadcast(num, pid, leader, &p, &lab, &i, &labr, &old_p, &old_lab, &old_i, &old_labr);
+                p = p + 1;
+            }
+            else {
+                p = p + 1;
+            } 
+        }
+        else {
+            p = p + 1;
+        } 
+    }
+    else {
+        p = p + 1;
+    }
+
+    lab = 1;
+    assert((p > old_p) || ((p == old_p) && (lab > old_lab)) || ((p == old_p) && (lab == old_lab) && (i > old_i)) || ((p == old_p) && (lab == old_lab) && (i == old_i) && (labr >= old_labr)));                                
+    
+}
+
 int main() {
     sendingThread(0, 0, 0, 0);
-    main_thread(0, 5); // pid, num
+    //main_thread(0, 5); // pid, num
+    leadership(5);
 }
