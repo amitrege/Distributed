bac53c65fbad1da9dfa5971648c1a0c4c9ad4c6e
Amit Kiran Rege <amitrege96@gmail.com>
2018-07-10 15:22:57 +0200

    Minor fix

--- abp_memcad.c
+++ abp_memcad.c
@@ -234,8 +234,7 @@ int AlternatingBit(int id) {
                     //retry = rand() % 2;
                     //rand(&retry);
                 }
-
-                assert(num_mbox_msg >= 1);
+                assert(retry6 != 0);
                 if (num_mbox_msg >= 1) {
                     lab = 4;
 
