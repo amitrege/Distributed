9b357c027bd6ea000eeb2a1e881a2a72a6a1fe12
Amit Kiran Rege <amitrege96@gmail.com>
2018-07-10 14:35:33 +0200

    Minor fix

--- abp_memcad.c
+++ abp_memcad.c
@@ -221,6 +221,7 @@ int AlternatingBit(int id) {
                 //rand(&retry);
                 
                 while (retry6 && num_mbox_msg < 1) {
+                    assert(0==0);
                     if (filter_msg_1(&m, count)) {
                         mbox_msg[num_mbox_msg] = &m;
                         num_mbox_msg = num_mbox_msg + 1;
