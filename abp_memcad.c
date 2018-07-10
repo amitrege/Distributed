55f4e662c9a435adc8361e82bf182430a521186b
Amit Kiran Rege <amitrege96@gmail.com>
2018-07-10 12:51:27 +0200

    Minor fix

--- abp_memcad.c
+++ abp_memcad.c
@@ -103,6 +103,7 @@ int AlternatingBit(int id) {
             }
 
             if (num_mbox_ack >= 1) {
+                assert(num_mbox_ack >= 1);
                 lab = 3;
 
                 assert((count > old_count) || ((count == old_count) && (lab > old_lab)));
