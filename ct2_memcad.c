b14525ac8666cde48c262e7e2002f84780ef4f62
Amit Kiran Rege <amitrege96@gmail.com>
2018-07-10 16:31:52 +0200

    Minor fix

--- ct2_memcad.c
+++ ct2_memcad.c
@@ -131,7 +131,7 @@ int propose(int pid, int num, int estimate) {
                 if(commit1) {
                     lab = 4;
 
-                    // assert lab == 4 || (round > old_round) || ((round == old_round) ==> lab >= old_lab);
+                    assert lab == 4 || (round > old_round) || ((round == old_round) ==> lab >= old_lab);
                     assert((lab == 4) || (round > old_round) || ((round == old_round) && (lab > old_lab)));
                     old_round = round;
                     old_lab = lab;
@@ -144,7 +144,7 @@ int propose(int pid, int num, int estimate) {
                 lab = 2;
 
                 // assert lab == 4 || (round > old_round) || ((round == old_round) ==> lab >= old_lab);
-                // assert((lab == 4) || (round > old_round) || ((round == old_round) && (lab > old_lab)));
+                assert((lab == 4) || (round > old_round) || ((round == old_round) && (lab > old_lab)));
                 old_round = round;
                 old_lab = lab;
 
@@ -249,7 +249,7 @@ int propose(int pid, int num, int estimate) {
                 lab = 4;
 
                 // assert lab == 4 || (round > old_round) || ((round == old_round) ==> lab >= old_lab);
-                assert((lab == 4) || (round > old_round) || ((round == old_round) && (lab > old_lab)));
+                // assert((lab == 4) || (round > old_round) || ((round == old_round) && (lab > old_lab)));
                 old_round = round;
                 old_lab = lab;
 
