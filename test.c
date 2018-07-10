d7dbe27d7420ead83eacd9cdaa0172cef91ad645
Amit Kiran Rege <amitrege96@gmail.com>
2018-07-10 14:06:53 +0200

    Minor fix

--- test.c
+++ test.c
@@ -1,14 +1,16 @@
 int main() {
     int r;
 
+    /*
     while (r) {
         // Do something
     }
-
+*/
+    _memcad("assume(r==0)");
     // r should be 0
     assert(r == 0);
     
-    _memcad("assume(r >= 0)");
+    _memcad("assume(r>=0)");
 
     // r should be >= 0
     assert(r >= 0);
