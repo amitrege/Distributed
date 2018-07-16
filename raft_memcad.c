3258279a0dc415229aa294bf9cf9a5013c847dba
Amit Kiran Rege <amitrege96@gmail.com>
2018-07-16 13:14:06 +0200

    Raft test

--- raft_memcad.c
+++ raft_memcad.c
@@ -567,8 +567,60 @@ void test (int pid, int num) {
     }
 }
 
+void test_2 () { 
+    int retry;
+    volatile int random;
+    int cmd;
+    int currentTerm = 0;
+    int lab_election = 0;
+    int commitIndex = 0;
+    int lab_normal = 0;
+    int lastIndex = 0;
+
+    int old_term = currentTerm - 1;
+    int old_lab_election = 0;
+    int old_commit = 0;
+    int old_lab_normal = 0;
+    int old_LLI = 0;
+
+    retry = random;
+    
+    while (retry) {
+        // receive command from client
+        cmd = random;
+        if (cmd == 0) {   // Empty command (HeartBeat)
+            lab_normal = 1;
+
+            assert ((currentTerm > old_term) || ((currentTerm == old_term) && (lab_election > old_lab_election)) || ((currentTerm == old_term && lab_election == old_lab_election) && (commitIndex > old_commit)) || ((currentTerm == old_term && lab_election == old_lab_election && commitIndex == old_commit) && (lab_normal > old_lab_normal)) || ((currentTerm == old_term && lab_election == old_lab_election && commitIndex == old_commit && lab_normal == old_lab_normal) && (lastIndex >= old_LLI)));            
+            old_term = currentTerm;
+            old_lab_election = lab_election;
+            old_commit = commitIndex;
+            old_lab_normal = old_lab_normal;
+            old_LLI = lastIndex;
+
+            // send(term, leaderId, prevLogIndex, entries[], leaderCommit) with empty entries
+        }
+        else {
+            lastIndex = lastIndex + 1;
+
+            lab_normal = 1;
+
+            assert ((currentTerm > old_term) || ((currentTerm == old_term) && (lab_election > old_lab_election)) || ((currentTerm == old_term && lab_election == old_lab_election) && (commitIndex > old_commit)) || ((currentTerm == old_term && lab_election == old_lab_election && commitIndex == old_commit) && (lab_normal > old_lab_normal)) || ((currentTerm == old_term && lab_election == old_lab_election && commitIndex == old_commit && lab_normal == old_lab_normal) && (lastIndex >= old_LLI)));            
+            old_term = currentTerm;
+            old_lab_election = lab_election;
+            old_commit = commitIndex;
+            old_lab_normal = old_lab_normal;
+            old_LLI = lastIndex;
+
+            // send(term, leaderId, prevLogIndex, entries[], leaderCommit)
+        }
+        retry = random;
+    }
+}
+
 int main() {
     //Raft(0,5);
-    test(0,5);
+    // test(0,5);
+    test_2();
     return 0;
 }
