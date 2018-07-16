eb80e9da26e81d9c91c7741bc8c32e50b9f35b2f
Amit Kiran Rege <amitrege96@gmail.com>
2018-07-16 16:50:20 +0200

    Raft test

--- raft_memcad.c
+++ raft_memcad.c
@@ -732,6 +732,7 @@ void follower_normal (int pid, int num) {
                 if (filter_AE(&m_AE, currentTerm, lastIndex, lastTerm)) {
                     // mbox_AE[num_mbox_AE] = m_AE;
                     num_mbox_AE = num_mbox_AE + 1;
+                    assert(0==0);
                 }
     
                 retry = random;
